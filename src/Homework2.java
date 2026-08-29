d34438511ee7d0115428fe15d3842197b804f2e3
lasersushi <lwalker30@priorypanther.com>
2026-08-29 10:55:44 -0700

    feat/chore: creating computeDivision, creating tests

--- src/Homework2.java
+++ src/Homework2.java
@@ -7,7 +7,9 @@ AT CS
 
 public class Homework2 {
     public static void main(String[]args) {
-        
+        int a = 5;
+        int b = 2;
+        System.out.println(computeDivision(a,b));
     }
     public static int computeQuotient(int dividend, int divisor) {
         int answer = dividend/divisor;
@@ -19,6 +21,15 @@ public class Homework2 {
     }
     public static double computeDecimal(int dividend, int divisor) {
         double answer = (double) dividend/divisor;
+        return answer;
+    }
+    public static String computeDivision(int dividend, int divisor) {
+        int a = dividend;
+        int b = divisor;
+        int c = dividend/divisor;
+        int d = dividend%divisor;
+        double e = (double) dividend/divisor;
+        String answer = a + "divided by" + b + "is equal to" + c + "remainder" + d + "or" + e;
         return answer;
     }
 }
