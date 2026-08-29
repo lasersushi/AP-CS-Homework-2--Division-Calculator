6a2d5386b96f4d26322189d326b80af9889dc757
lasersushi <lwalker30@priorypanther.com>
2026-08-29 16:13:50 -0700

    chore: Getting rid of test cases

--- src/Homework2.java
+++ src/Homework2.java
@@ -7,9 +7,7 @@ AT CS
 
 public class Homework2 {
     public static void main(String[]args) {
-        int a = 5;
-        int b = 2;
-        System.out.println(computeDivision(a,b));
+
     }
     public static int computeQuotient(int dividend, int divisor) {
         int answer = dividend/divisor;
