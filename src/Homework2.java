8dc1f43406c79de09a188fe60059d87f934a9b3f
lasersushi <lwalker30@priorypanther.com>
2026-08-29 10:31:11 -0700

    feat: Inital homework2.java class

---
+++ src/Homework2.java
@@ -0,0 +1,26 @@
+/*
+Lucas Walker
+Mr. Menezes
+AT CS
+8/29/26
+*/
+
+public class Homework2 {
+    public static void main(String[]args) {
+        int a = 5;
+        int b = 2;
+        computeDecimal(a, b);
+    }
+    public static int computeQuotient(int dividend, int divisor) {
+        int answer = dividend/divisor;
+        return answer;
+    }
+    public static int computeRemainder(int dividend, int divisor) {
+        int answer = dividend%divisor;
+        return answer;
+    }
+    public static double computeDecimal(int dividend, int divisor) {
+        double answer = dividend/divisor;
+        return answer;
+    }
+}
